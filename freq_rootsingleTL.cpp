30cb739406a5a8090da9da977179f75070faa7dd
lf-iacob <iacob.laura.f@gmail.com>
2026-07-23 12:49:14 +0200

    Update freq_rootsingleTL.cpp

--- freq_rootsingleTL.cpp
+++ freq_rootsingleTL.cpp
@@ -12,20 +12,23 @@ void freq_rootfile(string filecsv, TString output="output.root"){
      s: scintillator configuration
      150: trigger level (threshold)
      3: time window (minutes)
+     100: gate time (ms)
      sr90: type of radioactive source
   */
   string filename = fs::path(filecsv).filename().string();
   stringstream ss(filename);
-  string s, tl, tw, rads;
+  string s, tl, tw, g, rads;
   getline(ss, s, '_');
   getline(ss, tl, '_');
   getline(ss, tw, '_');
+  getline(ss, g, '_');
   getline(ss, rads, '.');
   cout<<endl<<"---------- FILE INFO ----------"<<endl
       <<"Type of radioactive source: "<<rads<<endl
       <<"Scintillators configuration: "<<s<<endl
       <<"Trigger level (mV): "<<tl<<endl
-      <<"Time window acquisition (min): "<<tw<<endl;
+      <<"Time window acquisition (min): "<<tw<<endl
+      <<"Gate time (ms): "<<g<<endl;
   int colour;
   if(stoi(tl)==100) colour=kPink-8;
   else if(stoi(tl)==150) colour=kViolet-2;
@@ -57,7 +60,7 @@ void freq_rootfile(string filecsv, TString output="output.root"){
 
   // ---------- Frequency HIST
   TCanvas *r = new TCanvas();
-  TH1F *fhist = new TH1F("fhist", "Frequency Histogram;Frequency (Hz);Entries",200,-10,160);
+  TH1F *fhist = new TH1F("fhist", "Frequency Histogram;Frequency (Hz);Entries",400,-10,160);
   fhist->SetLineColor(colour); 
   fhist->SetLineWidth(3);
   fhist->SetFillStyle(3004);
@@ -70,9 +73,9 @@ void freq_rootfile(string filecsv, TString output="output.root"){
   
   double f=fhist->GetMean(), f_err=fhist->GetMeanError();
  
-  TLegend *leg = new TLegend(0.49, 0.75, 0.88, 0.88);
+  TLegend *leg = new TLegend(0.25, 0.75, 0.88, 0.88);
   leg->SetTextSize(0.03);
-  leg->AddEntry(fhist, Form("%s (%s) - tl = %s mV; tw = %s min", rads.c_str(), s.c_str(), tl.c_str(), tw.c_str()));
+  leg->AddEntry(fhist, Form("%s (%s) - tl = %s mV; tw = %s min, gate = %s ms", rads.c_str(), s.c_str(), tl.c_str(), tw.c_str(), g.c_str()));
   leg->AddEntry(fhist, Form("f_{%s(%s),%s} = %.2f #pm %.2f Hz", rads.c_str(), s.c_str(), tl.c_str(), f, f_err), "");
   leg -> Draw();
   r->Update();
