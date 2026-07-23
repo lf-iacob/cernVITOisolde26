467de9f48ec0e4a05bd6a00fc863ce58d70dd56d
lf-iacob <iacob.laura.f@gmail.com>
2026-07-23 12:49:41 +0200

    Update freq_comp.cpp

--- freq_comp.cpp
+++ freq_comp.cpp
@@ -11,7 +11,7 @@ void freq_comp(const vector<string> &files, TString output = "comparison/output.
   // ---------- ROOT files Info
   size_t nFiles = files.size();
   if (nFiles == 0) cerr << "ERROR -- No input files provided!" << endl;
-  vector<string> s(nFiles), tl(nFiles), tw(nFiles), rads(nFiles), filenames(nFiles);
+  vector<string> s(nFiles), tl(nFiles), tw(nFiles), rads(nFiles), g(nFiles), filenames(nFiles);
   cout << endl << "---------- FILE INFO ----------" << endl;
   for (size_t i = 0; i < nFiles; ++i) {
     filenames[i] = fs::path(files[i]).filename().string();
@@ -19,26 +19,29 @@ void freq_comp(const vector<string> &files, TString output = "comparison/output.
     getline(ss_name, s[i], '_');
     getline(ss_name, tl[i], '_');
     getline(ss_name, tw[i], '_');
+    getline(ss_name, g[i], '_');
     getline(ss_name, rads[i], '.');
   }
   for (size_t i = 0; i < nFiles; ++i) {
     cout<<"File " << i + 1 << ":"<<endl
 	<<"  - Radioactive source: "<<rads[i]<<endl
 	<<"  - Scintillator config: "<<s[i]<<endl
-	<<"  - Trigger level (mV): "<<tl[i]<<endl;
+	<<"  - Trigger level (mV): "<<tl[i]<<endl
+	<<"  - Gate time (s): "<<g[i]<<endl;
   }
   cout<<endl;
   vector<Color_t> colours={kPink-8, kViolet-3, kAzure+2, kTeal+3, kOrange-3, kSpring-5};
 
   // ---------- new ROOT file
   if (output=="comparison/output.root") {
-    string s_comp="", tl_comp="", rad_comp="";
+    string s_comp="", tl_comp="", rad_comp="", g_comp="";
     for (size_t i=0; i<nFiles; ++i) {
       s_comp+=s[i];
       tl_comp+=tl[i];
       rad_comp+=rads[i];
+      g_comp+=g[i];
     }
-    output = "comparison/"+s_comp+"_"+tl_comp+"_"+rad_comp+".root";
+    output = "comparison/"+s_comp+"_"+tl_comp+"_"+g_comp+"_"+rad_comp+".root";
   }
   TFile *froot = new TFile(output, "RECREATE");
     
@@ -61,12 +64,13 @@ void freq_comp(const vector<string> &files, TString output = "comparison/output.
     } else h[i]->Draw("HIST F SAME");
   }
 
-  TLegend *leg = new TLegend(0.55, 0.55, 0.88, 0.88);
+  TLegend *leg = new TLegend(0.25, 0.55, 0.88, 0.88);
   leg->SetTextSize(0.03);
   for (size_t i = 0; i < nFiles; ++i) {
     double mean = h[i]->GetMean();
     double mean_err = h[i]->GetMeanError();
-    leg->AddEntry(h[i], Form("%s (%s) - tl = %s mV", rads[i].c_str(), s[i].c_str(), tl[i].c_str()));
+    leg->AddEntry(h[i], Form("%s (%s) - tl = %s mV (tw = %s min, g = %s ms)",
+			     rads[i].c_str(), s[i].c_str(), tl[i].c_str(), tw[i].c_str(), g[i].c_str()));
     leg->AddEntry(h[i], Form("f_{%s(%s),%s} = %.2f #pm %.2f Hz",
 			     rads[i].c_str(), s[i].c_str(), tl[i].c_str(), mean, mean_err), "");
   }
