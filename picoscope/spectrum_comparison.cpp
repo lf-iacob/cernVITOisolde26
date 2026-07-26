f1ae1ebacaf07383291d2c661c95c5b9efef8e34
lf-iacob <iacob.laura.f@gmail.com>
2026-07-26 20:05:00 +0200

    Update spectrum_comparison.cpp

--- picoscope/spectrum_comparison.cpp
+++ picoscope/spectrum_comparison.cpp
@@ -1,120 +1,112 @@
-// Energy beta spectrum: comparison between different files
+// Analysis beta decay: ENERGY SPECTRUM
 
 #include <iostream>
 #include <filesystem>
-#include <vector>
 using namespace std;
 namespace fs = filesystem;
+const int L=129;  //wf time window
 
-void spectrum_comp(const vector<string> &files, TString output = "comparison/output.root") {
+void spectrum_root(string file0, TString output="output.root"){
+  
+  // ---------- (h5->)txt Filename ----------
+  /* e.g. s1_100_10k_125_Sr90.txt
+     s1: scintillator configuration
+     100: trigger level (mV)
+     10k: number of waveforms
+     125: sampling rate (MS/s)
+     Sr90: radioactive source
+  */
+  string filename = fs::path(file0).filename().string();
+  stringstream ss(filename);
+  string s, tl, nw, sr, rads;
+  getline(ss, s, '_');
+  getline(ss, tl, '_');
+  getline(ss, nw, '_');
+  getline(ss, sr, '_');
+  getline(ss, rads, '.');
+  cout<<endl<<"---------- FILE INFO ----------"<<endl
+      <<"Type of radioactive source: "<<rads<<endl
+      <<"Scintillators configuration: "<<s<<endl
+      <<"Trigger level (mV): "<<tl<<endl
+      <<"Number of waveforms acquired: "<<nw<<endl
+      <<"Sampling rate (MS/s): "<<sr<<" -> Ticks (ns): "<<1000/stod(sr)<<endl<<endl;
+  int colour=kPink-8;
 
-  // ---------- ROOT files Info ----------
-  size_t nFiles = files.size();
-  if (nFiles == 0) cerr << "ERROR -- No input files provided!" << endl;
-  vector<string> s(nFiles), tl(nFiles), nw(nFiles), sr(nFiles), rads(nFiles), filenames(nFiles);
-  cout << endl << "---------- FILE INFO ----------" << endl;
-  for (size_t i = 0; i < nFiles; ++i) {
-    filenames[i] = fs::path(files[i]).filename().string();
-    stringstream ss_name(filenames[i]);
-    getline(ss_name, s[i], '_');
-    getline(ss_name, tl[i], '_');
-    getline(ss_name, nw[i], '_');
-    getline(ss_name, sr[i], '_');
-    getline(ss_name, rads[i], '.');
-  }
-  for (size_t i = 0; i < nFiles; ++i) {
-    cout<<"File " << i + 1 << ":"<<endl
-	<<"  - Radioactive source: "<<rads[i]<<endl
-	<<"  - Scintillator config: "<<s[i]<<endl
-	<<"  - Trigger level (mV): "<<tl[i]<<endl;
-  }
-  cout<<endl;
-  vector<Color_t> colours={kPink-8, kViolet-3, kAzure+2, kTeal+3, kOrange-3, kSpring-5};
-
-  // ---------- new ROOT file ----------
-  if (output=="comparison/output.root") {
-    string s_comp="", tl_comp="", rad_comp="";
-    for (size_t i=0; i<nFiles; ++i) {
-      s_comp+=s[i];
-      tl_comp+=tl[i];
-      rad_comp+=rads[i];
-    }
-    output = "comparison/"+s_comp+"_"+tl_comp+"_"+rad_comp+".root";
-  }
+  if(output=="output.root") output=fs::path(file0).stem().string()+".root";
   TFile *froot = new TFile(output, "RECREATE");
-    
-  // ---------- Charge Hist comparison ----------
-  gStyle->SetOptStat(00000);
+
+  // ---------- Read txt file -> ROOT File ----------
+  double a[L];
+  double b, c, amp;
+  TTree *dd = new TTree("dd", "dd");
+  dd->Branch("A", a, "a[129]/D");
+  dd->Branch("B", &b, "b/D");
+  dd->Branch("C", &c, "c/D");
+  dd->Branch("AMP", &amp, "amp/D");
+
+  ifstream file;
+  file.open(file0);
+  while(file.good()){
+    b=0.; c=0.; amp=0;
+    for(int i=0;i<L;i++){
+      file>>a[i]; 
+      if(i>=50 && i<=129) b+=double(a[i])/(129-50);
+      if((a[i]-b)>amp) amp = a[i]-b;
+      else if(i>=12 && i<=40) c+=(a[i]-b)*(1000/stod(sr))/50.; //LSB? Dynamic range of ADC? For volts conversion from ADC
+    }
+    dd->Fill();
+  }
+  file.close();
+ 
+  int n = dd->GetEntries();
+
+  // ---------- Charge Histogram ----------
+  TCanvas *cc = new TCanvas;
+  TH1F *hc = new TH1F("hc", "Charge Histogram;Charge (pC);Entries",200,0.02,0.5);
+  hc->SetLineColor(kRed-3); 
+  hc->SetLineWidth(3);
+  hc->SetFillStyle(3004);
+  hc->SetFillColor(kRed-3);
+  for(int i=0; i<n; i++) {
+    dd->GetEntry(i);
+    hc->Fill(c);
+  }
+  hc->Draw();
   
-  TCanvas *ccc = new TCanvas();
-  bool firstDrawn = false;
-  vector<TFile*> fl(nFiles);
-  vector<TH1F*> hc(nFiles);
-  for (int i=nFiles-1; i>=0; i--) {
-    fl[i] = TFile::Open(files[i].c_str(), "READ");
-    hc[i] = (TH1F*)fl[i]->Get("hc");
-    Color_t col=(i<colours.size())?colours[i]:(kBlack + i);
-    hc[i]->SetLineColor(col);
-    hc[i]->SetLineWidth(3);
-    hc[i]->SetFillStyle(3004);
-    hc[i]->SetFillColor(col);
-    if (!firstDrawn) {
-      hc[i]->Draw("HIST F");
-      firstDrawn = true;
-    } else hc[i]->Draw("HIST F SAME");
-  }
+  double c_mean=hc->GetMean(), c_err=hc->GetMeanError(), c_std=hc->GetStdDev();
 
-  TLegend *legc = new TLegend(0.6, 0.45, 0.88, 0.88);
-  double c_mean, c_err, c_std;
-  legc->SetTextSize(0.02);
-  for (size_t i=0; i<nFiles; i++) {
-    c_mean = hc[i]->GetMean();
-    c_err = hc[i]->GetMeanError();
-    c_std = hc[i]->GetStdDev();
-    legc->AddEntry(hc[i], Form("%s - Trigger Level = %s mV", rads[i].c_str(), tl[i].c_str()));
-    legc->AddEntry(hc[i], Form("#splitline{Charge_{%s,%s} = %.4f #pm %.4f pC}{(#sigma = %.2f pC)}",
-			       rads[i].c_str(), tl[i].c_str(), c_mean, c_err, c_std), "");
-  }
-  legc->Draw();
-  ccc->Update();
-
-
-  // ---------- Amplitude Hist comparison ----------
-  TCanvas *aaa = new TCanvas();
-  firstDrawn = false;
-  vector<TH1F*> hamp(nFiles);
-  for (int i=nFiles-1; i>=0; i--) {
-    fl[i] = TFile::Open(files[i].c_str(), "READ");
-    hamp[i] = (TH1F*)fl[i]->Get("hamp");
-    Color_t col=(i<colours.size())?colours[i]:(kBlack + i);
-    hamp[i]->SetLineColor(col);
-    hamp[i]->SetLineWidth(3);
-    hamp[i]->SetFillStyle(3004);
-    hamp[i]->SetFillColor(col);
-    if (!firstDrawn) {
-      hamp[i]->Draw("HIST F");
-      firstDrawn = true;
-    } else hamp[i]->Draw("HIST F SAME");
-  }
-
-  TLegend *lega = new TLegend(0.6, 0.45, 0.88, 0.88);
-  double amp_mean, amp_err, amp_std;
-  lega->SetTextSize(0.02);
-  for (size_t i=0; i<nFiles; i++) {
-    amp_mean = hc[i]->GetMean();
-    amp_err = hc[i]->GetMeanError();
-    amp_std = hc[i]->GetStdDev();
-    lega->AddEntry(hamp[i], Form("%s - Trigger Level = %s mV", rads[i].c_str(), tl[i].c_str()));
-    lega->AddEntry(hamp[i], Form("#splitline{Amplitude_{%s,%s} = %.4f #pm %.4f ADC}{(#sigma = %.2f ADC)}",
-				 rads[i].c_str(), tl[i].c_str(), amp_mean, amp_err, amp_std), "");
-  }
-  lega->Draw();
-  aaa->Update();
-
+  TLegend *leg_c = new TLegend(0.43, 0.7, 0.88, 0.88);
+  leg_c->SetTextSize(0.03);
+  leg_c->AddEntry(hc, Form("%s - %s - Trigger Level = %s mV", s.c_str(), rads.c_str(), tl.c_str()));
+  leg_c->AddEntry(hc, Form("#splitline{Charge_{%s,%s} = %.4f #pm %.4f pC}{(#sigma = %.2f pC)}",
+			   rads.c_str(), tl.c_str(), c_mean, c_err, c_std), "");
+  leg_c->Draw();
+  cc->Update();
   
-  cout << "Comparison shown in the plots." << endl << endl;
-  froot->cd();
-  aaa->Write();
-  ccc->Write();
+  // ---------- Amplitude Histogram ----------
+  TCanvas *ca = new TCanvas;
+  TH1F *hamp = new TH1F("hamp", "Amplitude Histogram;Amplitude (ADC);Entries",200,0.05,0.6);
+  hamp->SetLineColor(kAzure+2); 
+  hamp->SetLineWidth(3);
+  hamp->SetFillStyle(3004);
+  hamp->SetFillColor(kAzure+2);
+  for(int i=0; i<n; i++) {
+    dd->GetEntry(i);
+    hamp->Fill(amp);
+  }
+  hamp->Draw();
+  
+  double amp_mean=hamp->GetMean(), amp_err=hamp->GetMeanError(), amp_std=hamp->GetStdDev();
+  
+  TLegend *leg_amp = new TLegend(0.43, 0.7, 0.88, 0.88);
+  leg_amp->SetTextSize(0.03);
+  leg_amp->AddEntry(hamp, Form("%s - %s - Trigger Level = %s mV", s.c_str(), rads.c_str(), tl.c_str()));
+  leg_amp->AddEntry(hamp, Form("#splitline{Amplitude_{%s,%s} = %.4f #pm %.4f ADC}{(#sigma = %.2f ADC)}",
+			       rads.c_str(), tl.c_str(), amp_mean, amp_err, amp_std), "");
+  leg_amp->Draw();
+  ca->Update();
+  
+  
+
   froot->Write();
 }
